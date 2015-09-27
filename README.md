298868890dc9205752d9734fd379662c343d5408
luizumbra <luiz_fantasma@hotmail.com>
2015-09-27 17:52:26 -0300

    L

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# GettingAndCleaningData
+## GettingAndCleaningData
 Coursera project of the course Getting and Cleaning Data
+
+## run_analysis.R
+
+# First merge the x_train with x_test files
+
+# After that merge y_train with y_test files
+
+Remember this analysis must do train with test not test with train, because otherwise the merge of 4 files would drag wrong.
+
+## using lapply I extract the mean and the standard deviation of each column
--- run_analysis.R
+++ run_analysis.R
@@ -3,55 +3,44 @@ originalDir <- getwd()
 if(!grepl("UCI HAR Dataset$", getwd()))
   setwd(dir = "./UCI\ HAR\ Dataset/")
 
-# Exercise 1 - Merges the training and the test sets to create one data set.
-
-# merge the subject
-dataFrame1 <- read.table(file = "./test/subject_test.txt")
-dataFrame2 <- read.table(file = "./train/subject_train.txt")
-subject <- merge.data.frame(dataFrame1, dataFrame2)
+### Exercise 1 - Merges the training and the test sets to create one data set.
 
 # merge x
 dataFrame1 <- read.table(file = "./test/X_test.txt")
 dataFrame2 <- read.table(file = "./train/X_train.txt")
-x <- rbind(dataFrame1, dataFrame2)
+subject <- rbind(dataFrame1, dataFrame2)
 
 # merge y
 dataFrame1 <- read.table(file = "./test/Y_test.txt")
 dataFrame2 <- read.table(file = "./train/Y_train.txt")
-y <- rbind(dataFrame1, dataFrame2)
+namesDataset <- rbind(dataFrame1, dataFrame2)
 
-# merge acc x, y e z
-dataFrame1 <- read.table(file = "./test/Inertial\ Signals/body_acc_x_test.txt")
-dataFrame2 <- read.table(file = "./train/Inertial\ Signals/body_acc_x_train.txt")
-accX <- rbind(dataFrame1, dataFrame2)
-dataFrame1 <- read.table(file = "./test/Inertial\ Signals/body_acc_y_test.txt")
-dataFrame2 <- read.table(file = "./train/Inertial\ Signals/body_acc_y_train.txt")
-accY <- rbind(dataFrame1, dataFrame2)
-dataFrame1 <- read.table(file = "./test/Inertial\ Signals/body_acc_z_test.txt")
-dataFrame2 <- read.table(file = "./train/Inertial\ Signals/body_acc_z_train.txt")
-accZ <- rbind(dataFrame1, dataFrame2)
+### Exercise 2 - Extracts only the measurements on the mean and standard deviation for each measurement. 
 
-# merge gyro x, y, z
-dataFrame1 <- read.table(file = "./test/Inertial\ Signals/body_gyro_x_test.txt")
-dataFrame2 <- read.table(file = "./train/Inertial\ Signals/body_gyro_x_train.txt")
-gyroX <- rbind(dataFrame1, dataFrame2)
-dataFrame1 <- read.table(file = "./test/Inertial\ Signals/body_gyro_y_test.txt")
-dataFrame2 <- read.table(file = "./train/Inertial\ Signals/body_gyro_y_train.txt")
-gyroY <- rbind(dataFrame1, dataFrame2)
-dataFrame1 <- read.table(file = "./test/Inertial\ Signals/body_gyro_z_test.txt")
-dataFrame2 <- read.table(file = "./train/Inertial\ Signals/body_gyro_z_train.txt")
-gyroZ <- rbind(dataFrame1, dataFrame2)
+# Take the names of the dataset
+namesSubject <- names(subject)
 
-# merge total acc x, y, z
-dataFrame1 <- read.table(file = "./test/Inertial\ Signals/total_acc_x_test.txt")
-dataFrame2 <- read.table(file = "./train/Inertial\ Signals/total_acc_x_train.txt")
-totalX <- rbind(dataFrame1, dataFrame2)
-dataFrame1 <- read.table(file = "./test/Inertial\ Signals/total_acc_y_test.txt")
-dataFrame2 <- read.table(file = "./train/Inertial\ Signals/total_acc_y_train.txt")
-totalY <- rbind(dataFrame1, dataFrame2)
-dataFrame1 <- read.table(file = "./test/Inertial\ Signals/total_acc_z_test.txt")
-dataFrame2 <- read.table(file = "./train/Inertial\ Signals/total_acc_z_train.txt")
-totalZ <- rbind(dataFrame1, dataFrame2)
+# Calculate the mean for each dataset
+meanSubject <- lapply(namesSubject, function(x) {
+  m <- mean(subject[, x])
+  print(paste(x, "mean:", m))
+  m
+})
+
+# Calculate the standard deviation for each dataset
+sdSubject <- lapply(namesSubject, function(x) {
+  s <- sd(subject[, x])
+  print(paste(x, "standard deviation:", s))
+  s
+})
+
+### Exercise 3 - Uses descriptive activity names to name the activities in the data set
+
+# Put tha names activity
+subject <- cbind(namesDataset, subject)
+
+# Create .txt file
+write.table(subject, file = "./TidyData.txt", row.names = FALSE)
 
 # Return to the original directory
 if(!grepl(originalDir, getwd()))
